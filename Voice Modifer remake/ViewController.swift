adf4124232dd021754ef3575afb2954decea4ef8
Z <zamin>
2019-05-08 09:27:16 -0500

    Recreated part of the recorder

--- Voice Modifer remake/ViewController.swift
+++ Voice Modifer remake/ViewController.swift
@@ -7,14 +7,89 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,AVAudioPlayerDelegate, AVAudioRecorderDelegate {
+    
+    var AudioRecorder:AVAudioRecorder?
+    var AudioPlayer:AVAudioPlayer?
+    
+    let filename = "sound.caf"
+    
+    
+    @IBOutlet weak var StopButton: UIButton!
+    
+    @IBOutlet weak var PlayButton: UIButton!
+    
+    @IBOutlet weak var RecordButton: UIButton!
+    
+    @IBAction func StopAudio(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        PlayButton.isEnabled = false
+        StopButton.isEnabled = false
+        
+        let fileMgr = FileManager.default
+        
+        let dirPaths = fileMgr.urls(for: .documentDirectory,in: .userDomainMask)
+        
+        let soundFileURL = dirPaths[0].appendingPathComponent("sound.caf")
+        
+        let recordSettings =
+            [AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue,
+             AVEncoderBitRateKey: 16,
+             AVNumberOfChannelsKey: 2,
+             AVSampleRateKey: 44100.0] as [String : Any]
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        
+        // This do statement try's the audio session that we have created and also includes a catch statement that allows us to catch any errors we ahve
+        do {
+            try audioSession.setCategory(
+                AVAudioSession.Category.playAndRecord)
+        }
+        catch let error as NSError {
+            print("audioSession error: \(error.localizedDescription)")
+        }
+        
+        //This second try statment tests the audio recorder with the recording session that we have created earlier if it fails we catch the error
+        do {
+            try AudioRecorder =  AVAudioRecorder(url: soundFileURL,settings: recordSettings as [String: AnyObject])
+            AudioRecorder?.prepareToRecord()
+        }
+        catch let error as NSError {
+            print("audioSession error: \(error.localizedDescription)")
+            
+        }
+  }
+   
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        RecordButton.isEnabled = true
+        StopButton.isEnabled = false
     }
+    
+    func audioPlayerErrorOccured(_ player: AVAudioPlayer, error: Error?){
+        print("Audio Play Decode Error")
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        RecordButton.isEnabled = false
+        StopButton.isEnabled = true
+        PlayButton.isEnabled = true
+    }
+    
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        print("Audio Record Encode Error")
+    }
+    
+    
+  }
 
 
-}
+
+
+
+
 
