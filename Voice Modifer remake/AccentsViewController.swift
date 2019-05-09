368700e9bb7bc4c5a3d45257e38445475f162187
Z <zamin>
2019-05-09 13:23:29 -0500

    Just finished the accent view controller for the different languages. really long process

--- Voice Modifer remake/AccentsViewController.swift
+++ Voice Modifer remake/AccentsViewController.swift
@@ -12,32 +12,72 @@ class AccentsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
     }
     
     @IBAction func GermanPhrases(_ sender: Any) {
-        let synthesizer = AVSpeechSynthesizer()
-        var accent = AVSpeechUtterance(string: "")
-        var dialogue = ["Hello Chap","Man I was buzzin at the bar after drinks", "Man it chucking down today, looks like no futbol","I’m gutted, man, I didn’t even have the chance to get her phone number.", ]
-        accent.voice = AVSpeechSynthesisVoice(language: "en-GB-Daniel")
-        accent = AVSpeechUtterance(string: dialogue.randomElement()!)
-        accent.rate = 0.4
-        synthesizer.speak(accent)
+        
+   
+       let synthesizer1 = AVSpeechSynthesizer()
+       let synthesizer2 = AVSpeechSynthesizer()
+      let synthesizer3 = AVSpeechSynthesizer()
+        let accent1 = AVSpeechUtterance(string: "Darf ich mal vorbei?")
+        let accent2 = AVSpeechUtterance(string: "Hallo!")
+         let accent3 = AVSpeechUtterance(string: "Guten Morgen!")
+        accent1.voice = AVSpeechSynthesisVoice(language: "de-DE")
+        accent2.voice = AVSpeechSynthesisVoice(language: "de-DE")
+        accent3.voice = AVSpeechSynthesisVoice(language: "de-DE")
+        accent1.rate = 0.4
+        accent2.rate = 0.4
+        accent3.rate = 0.4
+        synthesizer1.speak(accent1)
+         synthesizer2.speak(accent2)
+        synthesizer3.speak(accent3)
+        
+        
     }
     
     @IBAction func EnglishPhrases(_ sender: UIButton) {
-        let synthesizer = AVSpeechSynthesizer()
-        var accent = AVSpeechUtterance(string: "")
-        var dialogue = ["Hello Chap","Man I was buzzin at the bar after drinks", "Man it chucking down today, looks like no futbol","I’m gutted, man, I didn’t even have the chance to get her phone number.", ]
-        accent.voice = AVSpeechSynthesisVoice(language: "en-GB-Daniel")
-        accent = AVSpeechUtterance(string: dialogue.randomElement()!)
-        accent.rate = 0.4
-        synthesizer.speak(accent)
+        
+       let synthesizer1 = AVSpeechSynthesizer()
+        let synthesizer2 = AVSpeechSynthesizer()
+        let synthesizer3 = AVSpeechSynthesizer()
+        
 
+       let accent1 = AVSpeechUtterance(string: "How are doing today mate?")
+         let accent2 = AVSpeechUtterance(string: "cheers mate")
+        let accent3 = AVSpeechUtterance(string: "Not my cuppa tea")
+       
+        accent1.voice = AVSpeechSynthesisVoice.init(language:"en-GB")
+        accent2.voice = AVSpeechSynthesisVoice.init(language:"en-GB")
+       accent3.voice = AVSpeechSynthesisVoice.init(language:"en-GB")
+        accent1.rate = 0.4
+        accent2.rate = 0.4
+        accent3.rate = 0.4
+        synthesizer1.speak(accent1)
+        synthesizer2.speak(accent2)
+        synthesizer3.speak(accent3)
     }
     
     
     @IBAction func SpanishPhrases(_ sender: UIButton) {
+        let synthesizer1 = AVSpeechSynthesizer()
+         let synthesizer2 = AVSpeechSynthesizer()
+         let synthesizer3 = AVSpeechSynthesizer()
+        
+        let accent1 = AVSpeechUtterance(string: "Hola como estas, me llamo elizabeth")
+        let accent2 = AVSpeechUtterance(string: "¡Buenos días, Estela!")
+        let accent3 = AVSpeechUtterance(string: "Que tale mi amigo")
+        
+        accent1.voice = AVSpeechSynthesisVoice(language: "es-ES")
+          accent2.voice = AVSpeechSynthesisVoice(language: "es-ES")
+        accent3.voice = AVSpeechSynthesisVoice(language: "es-ES")
+        accent1.rate = 0.4
+        accent2.rate = 0.4
+        accent3.rate = 0.4
+        synthesizer1.speak(accent1)
+        synthesizer2.speak(accent2)
+        synthesizer3.speak(accent3)
     }
     
 }
